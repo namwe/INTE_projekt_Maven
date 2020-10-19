51fea1b4c6a1907a2aadf16e0263dfa47f0e3def
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-19 17:36:45 +0200

    Created Door singleton class

---
+++ src/main/java/level/Door.java
@@ -0,0 +1,15 @@
+package level;
+
+public class Door extends RoomTile{
+
+    public static final char CHAR_REPRESENTATION = '@';
+    private static Door instance = new Door();
+
+    private Door() {
+        super(CHAR_REPRESENTATION,false);
+    }
+
+    public static Door getInstance() {
+        return instance;
+    }
+}
