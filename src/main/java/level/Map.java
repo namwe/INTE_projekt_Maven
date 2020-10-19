651123232756189c14504a7508ab7a24569adbb4
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-19 17:32:06 +0200

    Changed size constants

--- src/main/java/level/Map.java
+++ src/main/java/level/Map.java
@@ -2,7 +2,7 @@ package level;
 
 
 public class Map {
-    public static final int WIDTH = 3, HEIGHT = 3;
+    public static final int WIDTH = 2, HEIGHT = 2;
     private Room[][] rooms;
 
     public Map(Room[][] rooms) {
