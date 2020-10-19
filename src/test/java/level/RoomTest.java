023d0f7b5a793abba32b31aba2a9f03153e9bd00
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-19 12:10:15 +0200

    Created test cases for Room.

--- src/test/java/level/RoomTest.java
+++ src/test/java/level/RoomTest.java
@@ -4,5 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class RoomTest {
+    @Test
+    void constructorSetsLayoutFromTemplate(){
+        Room r = new Room(Room.generateRoomWithWalls());
+        assertArrayEquals(Room.generateRoomWithWalls(),r.getLayout());
+    }
 
+    @Test
+    void generateRoomWithWalls(){
+        RoomTile[][] roomWithWalls = Room.generateRoomWithWalls();
+
+        RoomTile[] wallRow = new RoomTile[9];
+        for (int i = 0; i < wallRow.length; i++) {
+            wallRow[i] = new Wall();
+        }
+
+        assertArrayEquals(wallRow, roomWithWalls[0]);
+        assertArrayEquals(wallRow, roomWithWalls[8]);
+
+        RoomTile[] airRow = wallRow.clone();
+        for (int i = 1; i < airRow.length-1; i++) {
+            airRow[i] = new Air();
+        }
+
+        for (int i = 1; i < roomWithWalls.length-1; i++) {
+            assertArrayEquals(airRow, roomWithWalls[i]);
+        }
+    }
 }
