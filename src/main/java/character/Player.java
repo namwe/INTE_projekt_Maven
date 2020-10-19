f76cc94d8f17350ff31315d9537be42acc2d2266
Herman Westerman <hewe1050@student.su.se>
2020-10-19 15:31:49 +0200

    Make implementation 'green'

--- src/main/java/character/Player.java
+++ src/main/java/character/Player.java
@@ -27,6 +27,10 @@ public class Player {
         }
     }
 
+    public int getSpeed() {
+        return speed;
+    }
+
     public int getSizeOfInventory() {
         return equipments.size();
     }
