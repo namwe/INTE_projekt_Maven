a10c09691212548be9c81866b23eda971d35a70b
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-19 15:05:41 +0200

    Removed all code from Map

--- src/main/java/level/Map.java
+++ src/main/java/level/Map.java
@@ -4,19 +4,5 @@ import java.util.HashMap;
 
 public class Map {
 
-    private java.util.Map<RoomType, RoomTile[][]> roomTemplates = new HashMap<>();
-
-    private RoomTile[][] getRoomTemplate(RoomType type){
-        if (roomTemplates.isEmpty()) {
-            generateRoomTemplates();
-        }
-        return roomTemplates.get(type);
-    }
-
-    private void generateRoomTemplates() {
-        roomTemplates.put(RoomType.DEFAULT_ROOM, Room.generateRoomWithWalls());
-    }
-
-
 
 }
