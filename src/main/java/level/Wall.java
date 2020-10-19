08721b7fa337350d49286897c1228c3e901dd979
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-19 14:37:16 +0200

    Made Air & Wall into singleton classes

--- src/main/java/level/Wall.java
+++ src/main/java/level/Wall.java
@@ -3,8 +3,13 @@ package level;
 public class Wall extends RoomTile{
 
     public static final char CHAR_REPRESENTATION = '#';
+    private static Wall instance = new Wall();
 
-    public Wall() {
+    private Wall() {
         super(CHAR_REPRESENTATION,true);
+    }
+
+    public static Wall getInstance() {
+        return instance;
     }
 }
