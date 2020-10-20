3d6706a56d83d6b0573df6b697468250373c326c
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-20 14:15:18 +0200

    TDD, Position, constructor, red

---
+++ src/main/java/level/Position.java
@@ -0,0 +1,22 @@
+package level;
+
+public class Position {
+    public Position(int roomRow, int roomColumn, int tileRow, int tileColumn) {
+    }
+
+    public int getTileColumn() {
+        return -1;
+    }
+
+    public int getTileRow() {
+        return -1;
+    }
+
+    public int getRoomColumn() {
+        return -1;
+    }
+
+    public int getRoomRow() {
+        return -1;
+    }
+}
