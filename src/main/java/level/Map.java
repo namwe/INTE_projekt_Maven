80495927277593d5e7eaead9510f2343b74d61bd
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-20 12:30:36 +0200

    TDD, Map, constructor, refactored.

--- src/main/java/level/Map.java
+++ src/main/java/level/Map.java
@@ -6,7 +6,23 @@ public class Map {
     private Room[][] rooms;
 
     public Map(Room[][] rooms) {
+        if (!dimensionsAreOk(rooms)){
+            throw new IllegalArgumentException();
+        }
         this.rooms = rooms;
+    }
+
+    private boolean dimensionsAreOk(Room[][] rooms) {
+        if (rooms.length != HEIGHT) {
+            return false;
+        }
+
+        for (Room[] row : rooms) {
+            if (row.length != WIDTH) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public Room[][] getRooms() {
