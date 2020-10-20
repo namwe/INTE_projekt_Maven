7e0ed9d060ddb66555ba7c2f47dbe9fef56c3332
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-20 15:34:37 +0200

    Movable no longer extends RoomTile.

--- src/main/java/level/Movable.java
+++ src/main/java/level/Movable.java
@@ -1,10 +1,9 @@
 package level;
 
-public abstract class Movable extends RoomTile{
-    private RoomTile originalTile;
-    private int[][] roomPosition;
+public abstract class Movable{
+    private Position position;
 
-    public Movable(char charRepresentation) {
-        super(charRepresentation, true);
+    public Movable(Position position) {
+        this.position = position;
     }
 }
