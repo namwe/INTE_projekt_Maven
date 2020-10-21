e8222f3e4fff5b93b5ec65ba0a2ca6eef28c46ce
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-21 11:06:29 +0200

    Movable extends RoomTile

--- src/main/java/level/Movable.java
+++ src/main/java/level/Movable.java
@@ -1,10 +1,13 @@
 package level;
 
-public abstract class Movable{
+public abstract class Movable extends RoomTile{
     private Position position;
+    private Map map;
 
-    public Movable(Position position) {
+    public Movable(char charRepresentation, boolean solid, Position position, Map map) {
+        super(charRepresentation, solid);
         this.position = position;
+        this.map = map;
     }
 
     public Position getPosition() {
