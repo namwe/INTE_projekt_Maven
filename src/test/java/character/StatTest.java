c21441fd69a7ac766e6e3b89160d9db0d2c578ce
Herman Westerman <hewe1050@student.su.se>
2020-10-21 18:58:20 +0200

    Make tests 'green'

--- src/test/java/character/StatTest.java
+++ src/test/java/character/StatTest.java
@@ -14,4 +14,23 @@ class StatTest {
         });
     }
 
+    @Test
+    public void Throw_IAE_When_Mana_Set_To_Higher_Than_One_Hundred() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Stat stat = new Stat(5,120);
+        });
+    }
+
+    @Test
+    public void getHp_Returns_Five_When_Constructor_Is_Set_To_Five() {
+        Stat stat = new Stat(5,20);
+        assertEquals(5, stat.getHp());
+    }
+
+    @Test
+    public void getMana_Returns_Seven_When_Constructor_Is_Set_To_Seven() {
+        Stat stat = new Stat(12,7);
+        assertEquals(7, stat.getMana());
+    }
+
 }
