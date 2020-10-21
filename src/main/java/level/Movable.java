56a4408d9690ae809fd7240678636e8d15c93d58
Fredrik Engström <fredrik00engstrom@gmail.com>
2020-10-21 12:56:39 +0200

    Created model code for Movable.

--- src/main/java/level/Movable.java
+++ src/main/java/level/Movable.java
@@ -1,16 +1,40 @@
 package level;
 
-public abstract class Movable extends RoomTile{
+public abstract class Movable extends RoomTile {
     private Position position;
     private Map map;
+    private RoomTile originalTile;
 
-    public Movable(char charRepresentation, boolean solid, Position position, Map map) {
-        super(charRepresentation, solid);
+    public Movable(char charRepresentation, Position position, Map map) {
+        super(charRepresentation, true);
         this.position = position;
         this.map = map;
+        originalTile = map.replaceTile(position, this);
     }
 
     public Position getPosition() {
         return position;
+    }
+
+    public void move(Direction direction) {
+        Position nextPosition = position.getAdjacentPosition(direction);
+
+        if (!allowedMovment(nextPosition)) {
+            return;
+        }
+
+        map.replaceTile(position, originalTile);
+        position = nextPosition;
+        originalTile = map.replaceTile(nextPosition, this);
+    }
+
+    private boolean allowedMovment(Position nextPosition) {
+        RoomTile nextTile;
+        try {
+            nextTile = map.getTile(nextPosition);
+        } catch (IndexOutOfBoundsException ex) {
+            return false;
+        }
+        return nextTile.isSolid();
     }
 }
