8d852502cf13877e3da7ea2dd762b6967128122c
Herman Westerman <hewe1050@student.su.se>
2020-10-25 14:14:58 +0100

    Remove misleading test

--- src/test/java/character/ArmorTest.java
+++ src/test/java/character/ArmorTest.java
@@ -12,12 +12,4 @@ class ArmorTest {
         assertTrue(equipment.isEquipped());
     }
 
-    @Test
-    public void Armor_With_Same_Stats_And_Not_Equipped_Are_Not_Same_Object() {
-        Equipment equipment = new Armor(new StatEquipment(3,7));
-        Equipment equipment2 = new Armor(new StatEquipment(3,7));
-        equipment.setEquipped(true);
-        assertEquals(1, equipment.compareTo(equipment2));
-    }
-
 }
