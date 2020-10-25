d66fe97366e1e3b3a8a450b1ba9993795d96ac1e
Herman Westerman <hewe1050@student.su.se>
2020-10-25 14:14:06 +0100

    Remove misleading test

--- src/test/java/character/SwordTest.java
+++ src/test/java/character/SwordTest.java
@@ -7,13 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class SwordTest {
 
     @Test
-    public void Sword_With_Same_Stats_And_Not_Equipped_Are_Same_Object() {
-        Equipment equipment = new Sword(new StatEquipment(3,7));
-        Equipment equipment2 = new Sword(new StatEquipment(3,7));
-        assertEquals(0, equipment.compareTo(equipment2));
-    }
-
-    @Test
     public void Sword_Is_Equipped() {
         Equipment equipment = new Sword(new StatEquipment(2,5));
         equipment.setEquipped(true);
