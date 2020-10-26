27ea14221681b044af1be2be0dc8d0d16d79d628
BodilLof <bodil.lofholm@gmail.com>
2020-10-26 23:00:54 +0100

    Now

--- src/main/java/monster/Now.java
+++ src/main/java/monster/Now.java
@@ -3,10 +3,10 @@ package monster;
 import java.util.Calendar;
 
 public class Now {
-	private boolean night; // privat?
-	private boolean midnight = false; // privat?
+	private boolean night; 
+	private boolean midnight = false; 
 	private static Now INSTANCE = null;
-
+	
 	public static Now getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new Now();
@@ -14,18 +14,30 @@ public class Now {
 		return INSTANCE;
 	}
 
-	//annan konstruktor?? 
 	
-	protected Now() { // ok med protected för att kunna skapa upp mock-instanser i subklasserna?
-		Calendar rightNow = Calendar.getInstance();
-		int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+	protected Now() { 
+		Calendar c = Calendar.getInstance();
+		setNight(c);
+		setMidnight(c);
+	}
+	
+	protected Now(int hour) { 
+		
+	}
+	
+	
+	private void setNight(Calendar c){
+		int hour = c.get(Calendar.HOUR_OF_DAY);
 		if (hour < 5 || hour > 21) // "natt" mellan 5 på morgonen och 22 på kvällen
-			this.night = true;
-		if (hour == 24) {
-			midnight = true;
-		}
+			this.night = true;	
 	}
 
+	private void setMidnight(Calendar c){
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		if (hour == 24)
+			midnight = true;			
+	}
+	
 	
 	public boolean isNight() {
 		return night;
